e85fb049b6b3cf701e479f3f82cca5bc89010f38
ERRYFRESH <errico.annunziata@gmail.com>
2023-11-20 21:42:47 +0100

    Lavorando sul main per bottoni

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,7 +18,7 @@ public class Main {
     static class EchoBot extends TelegramLongPollingBot {
         @Override
         public String getBotToken(){
-            return tgBot.getBotToken(); //Cusom class to provide telegram bot token
+            return tgBot.getBotToken(); //Custom class to provide telegram bot token
         }
 
         public void print(String txt){
@@ -43,7 +43,8 @@ public class Main {
                     ikm.setKeyboard(mainMenu());
                     SendMessage sendMessage = new SendMessage();
 
-                    sendMessage.setText("Choose an operation:");
+                    sendMessage.setText("Welcome to Bot-Hoveen, the first bot that allows you to create music from a phrase!\n" +
+                            "In the menu below you can customize the result or start the simulation, enjoy!");
                     sendMessage.setChatId(chatId);
                     sendMessage.setReplyMarkup(ikm);
 
@@ -60,42 +61,66 @@ public class Main {
                 String data = cbQuery.getData();
                 long id = cbQuery.getMessage().getChatId();
                 print(data);
-
-                char option = data.split(":")[0].charAt(0);
+                //ricavo il menu
+                char type = data.split(":")[0].charAt(0);
+                char option = data.split(":")[1].charAt(0);
+                System.out.println(option);
                 InlineKeyboardMarkup ikm= new InlineKeyboardMarkup();
 
                 SendMessage send = new SendMessage();
+                    switch (option){
+                        case '0':
+                            if(type == 'm'){
+                                ikm.setKeyboard(Instrument());
+                                send.setText("Choose which instrument to play on :");//funzione che accetta come parametro il numero affiancato alla lettera e crea il sottomenù corrispettivo
+                                //es m:1 ti porta al menù dei generi
+                            } else if (type == 'i') {
+                                ikm.setKeyboard(mainMenu());
+                                send.setText("Set the piano instrument, continue to customize the composer or press the \"Start\" button to start playing!");
+                            } else if (type == 'g') {
+                                ikm.setKeyboard(mainMenu());
+                                send.setText("Set the pop genre, continue to customize the composer or press the \"Start\" button to start playing!");
+                            }else {
+                                //battuta personalizzata
+                            }
+                            break;
+                        case '1':
+                            if(type == 'm'){
+                                ikm.setKeyboard(Genre());
+                                send.setText("Choose the music genre you prefer :");
+                            } else if (type == 'i') {
 
-                switch (option){
-                    case 'm':
-                        ikm.setKeyboard(/*funz*/); //funzione che accetta come parametro il numero affiancato alla lettera e crea il sottomenù corrispettivo
-                        //es m:1 ti porta al menù dei generi
-                        send.setText("");
-                        break;
-                    case 'i':
-                        ikm.setKeyboard(/*funz*/);
-                        send.setText("");
-                        break;
-                    case 'g':
-                        ikm.setKeyboard(/*funz*/);
-                        send.setText("");
-                        break;
-                    case 'a':
-                        ikm.setKeyboard(/*funz*/);
-                        send.setText("");
-                        break;
-                    case 'r'://return e richiama mainMenu()
-                        ikm.setKeyboard(mainMenu());
-                        send.setText("Choose an operation:");
-                        break;
-                }
+                            } else if (type == 'g') {
+
+                            }else{
+
+                            }
+                            break;
+                        case '2':
+                            ikm.setKeyboard(AdSettings());
+                            send.setText("Are you a real expert? \n" +
+                                    "Well have fun customizing to the best of your ability!");
+                            break;
+                        case '3'://dovrebbe essere lo start
+                            //ikm.setKeyboard(/*funz*/);
+                            send.setText("Are you a real expert? \n" +
+                                    "Well have fun customizing to the best of your ability!");
+                            break;
+                        case '4'://return e richiama mainMenu()
+                            ikm.setKeyboard(mainMenu());
+                            send.setText("Ready to get started?\n" +
+                                    "Hit the \"Start\" button and enter your phrase, otherwise continue to customize your result!");
+                            break;
+                    }
+
+
 
 
                 send.setChatId(id);
                 send.setReplyMarkup(ikm);
 
-                send.setChatId(id);
-                send.setText("PRESS- "+data);
+                //send.setChatId(id);
+                //send.setText("PRESS- "+data);
 
                 try{
                     execute(send);
@@ -143,13 +168,124 @@ public class Main {
             inlineButtons.add(buttons2);
             return inlineButtons;
         }
-    
+
+        private List<List<InlineKeyboardButton>> Instrument(){
+            List<List<InlineKeyboardButton>> inlineButtons = new ArrayList<>();
+            List<InlineKeyboardButton> buttons1 = new ArrayList<>();
+            List<InlineKeyboardButton> buttons2 = new ArrayList<>();
+
+            InlineKeyboardMarkup ikm= new InlineKeyboardMarkup();
+            SendMessage send = new SendMessage();
+
+            for(int i = 0; i<4; i++){
+                InlineKeyboardButton tmp = new InlineKeyboardButton();
+                String in = "";
+
+                switch (i){
+                    case 0:
+                        in="Piano";
+                        break;
+                    case 1:
+                        in = "Guitar";
+                        break;
+                    case 2:
+                        in = "Flute";
+                        break;
+                    case 3:
+                        in = "Return";
+                        ikm.setKeyboard(mainMenu());
+                        break;
+                    default:
+                        break;
+                }
+                tmp.setText(in);
+                tmp.setCallbackData("i:"+String.valueOf(i));
+                //non so perche
+                if(i<2) buttons1.add(tmp);
+                else buttons2.add(tmp);
+            }
+            inlineButtons.add(buttons1);
+            inlineButtons.add(buttons2);
+            return inlineButtons;
+
+            //send.setChatId(id);
+        }
+
+        private List<List<InlineKeyboardButton>> Genre(){
+            List<List<InlineKeyboardButton>> inlineButtons = new ArrayList<>();
+            List<InlineKeyboardButton> buttons1 = new ArrayList<>();
+            List<InlineKeyboardButton> buttons2 = new ArrayList<>();
+
+            for(int i=0; i<3; i++){
+                InlineKeyboardButton tmp = new InlineKeyboardButton();
+                String in = "";
+
+                switch (i){
+                    case 0:
+                        in = "Pop";
+                        break;
+                    case 1:
+                        in = "Rock";
+                        break;
+                    case 2:
+                        in = "Return";
+                        break;
+                    default:
+                        break;
+                }
+                tmp.setText(in);
+                tmp.setCallbackData("g:"+String.valueOf(i));
+                //non so perche
+                if (i<2) buttons1.add(tmp);
+                else buttons2.add(tmp);
+            }
+            inlineButtons.add(buttons1);
+            inlineButtons.add(buttons2);
+            return inlineButtons;
+        }
+
+        private List<List<InlineKeyboardButton>> AdSettings(){
+            List<List<InlineKeyboardButton>> inlineButtons = new ArrayList<>();
+            List<InlineKeyboardButton> buttons1 = new ArrayList<>();
+            List<InlineKeyboardButton> buttons2 = new ArrayList<>();
+
+            for(int i = 0; i < 4; i++){
+                InlineKeyboardButton tmp = new InlineKeyboardButton();
+                String in = "";
+
+                switch (i){
+                    case 0:
+                        in = "Custom arrangement";
+                        break;
+                    case 1:
+                        in = "Custom musical tempo";
+                        break;
+                    case 2:
+                        in = "Custom pattern";
+                        break;
+                    case 3:
+                        in = "Return";
+                        break;
+                    default:
+                        break;
+                }
+                tmp.setText(in);
+                tmp.setCallbackData("a:"+String.valueOf(i));
+                //non so perche
+                if (i<2) buttons1.add(tmp);
+                else buttons2.add(tmp);
+            }
+            inlineButtons.add(buttons1);
+            inlineButtons.add(buttons2);
+            return inlineButtons;
+        }
+
         @Override
         public String getBotUsername() {
             return "Musimatic_bot";
         }
 
-
+        //funzione pappagallo
         private SendMessage sendBotMessage(String text, String chatId){
             SendMessage sendMessage = new SendMessage();
             sendMessage.setChatId(chatId);
