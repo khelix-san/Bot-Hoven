c4a2460fcdefa64e60c31a80b6e32d5a4c70e511
khelix-san <82376512+khelix-san@users.noreply.github.com>
2023-11-21 17:19:12 +0100

    Creata classe User.java, adattati alcuni campi della classe Composer.java ed automatizzata la creazione della hashMap prossimo step: sostituzione metodo main con uno adhoc chiamabile dalle altre classi, metodo di modifica parametri in base alle preferenze utente, creazione musica secondo durate e note consonatni, creazione e conversione file

--- src/main/java/Composer.java
+++ src/main/java/Composer.java
@@ -4,26 +4,64 @@ import org.jfugue.player.Player;
 
 public class Composer {
     static String text = "ciao mamma guarda";
-    static String vocal = "aeiou";
+    static String patternLetters = "aeiou";
     static String word = "ABCDEFG";
-    static String ottave = "345";
+    static String octave = "345";
 
-    static String[] armonia = new String[]{"Cmaj", "Amin", "Fmaj", "Gmaj", "Cmaj", "Amin", "Dmin", "Gdom7"};
-    static String durate = "qis";
+    static ArrayList<String> armonia = new ArrayList<>(Arrays.asList("Cmaj", "Amin", "Fmaj", "Gmaj", "Cmaj", "Amin", "Dmin", "Gdom7"));
+    static String duration = "qis";
 
-    static  HashMap<String, String[]> note = new HashMap<String, String[]>();
+    static  HashMap<String, ArrayList<String>> note = new HashMap<String, ArrayList<String>>();
     static Random ran = new Random();
+
+    public static void hashmapMaker(){
+        int patternSize = patternLetters.length();
+        int octaveSize = octave.length();
+        int notesSize = word.length();
+        int currentOctave = octave.charAt(0) - '0';
+
+        for(int i=0;i<patternSize;i++)
+            note.put(String.valueOf(patternLetters.charAt(i)),new ArrayList<String>());
+
+        for(int i=0;i< octaveSize*notesSize;i++){
+            if(i%7==0 && i!=0) {
+                ++currentOctave;
+            }
+
+            ArrayList<String> tmp = note.get(String.valueOf(patternLetters.charAt(i%patternSize)));
+            tmp.add(word.charAt(i%notesSize)+""+currentOctave);
+            note.put(String.valueOf(patternLetters.charAt(i%patternSize)),tmp);
+        }
+    }
+
+    public static void printNoteMap(Map<String, ArrayList<String>> note) {
+        Map<String, ArrayList<String>> sortedNote = new TreeMap<>(note);
+
+        for (Map.Entry<String, ArrayList<String>> entry : sortedNote.entrySet()) {
+            String key = entry.getKey();
+            ArrayList<String> values = entry.getValue();
+
+            System.out.print("Key: " + key + ", Values: ");
+            for (String value : values) {
+                System.out.print(value + " ");
+            }
+            System.out.println(); // Move to the next line after printing values for a key
+        }
+    }
 
     public static void main (String[] args){
 
         Player pl = new Player();
 
-        note.put("a", new String[]{"E3", "C4", "A5", "F5", "R"});
-        note.put("e", new String[]{"F3", "D4", "B5", "G5", "R"});
-        note.put("i", new String[]{"G3", "E4", "C5", "A6"});
-        note.put("o", new String[]{"A4", "F4", "D5", "B6"});
-        note.put("u", new String[]{"B4", "G4", "E5", "C6"});
+        /*note.put("a", new ArrayList<>(Arrays.asList("E3", "C4", "A5", "F5", "R")));
+        note.put("e", new ArrayList<>(Arrays.asList("F3", "D4", "B5", "G5", "R")));
+        note.put("i", new ArrayList<>(Arrays.asList("G3", "E4", "C5", "A6")));
+        note.put("o", new ArrayList<>(Arrays.asList("A4", "F4", "D5", "B6")));
+        note.put("u", new ArrayList<>(Arrays.asList("B4", "G4", "E5", "C6")));
+        */
 
+        hashmapMaker();
+        printNoteMap(note);
 
         String music = "";
 
@@ -31,14 +69,14 @@ public class Composer {
 
         for (int i=0; i<text.length(); i++) {
             String c = text.charAt(i)+"";
-            if (vocal.contains(c)) {
-                int d = ran.nextInt(durate.length());
-                int a = ran.nextInt(armonia.length);
-                int o = ran.nextInt(ottave.length());
-                int r = ran.nextInt(note.get(c).length);
-                music = music + " "+note.get(c)[r]+"+"+armonia[j] + durate.charAt(d)+ottave.charAt(o);
+            if (patternLetters.contains(c)) {
+                int d = ran.nextInt(duration.length());
+                int a = ran.nextInt(armonia.size());
+                int o = ran.nextInt(octave.length());
+                int r = ran.nextInt(note.get(c).size());
+                music = music + " "+note.get(c).get(r)+"+"+armonia.get(j) + duration.charAt(d)+ octave.charAt(o);
                 j++;
-                if (j>=armonia.length) j=0;
+                if (j>=armonia.size()) j=0;
             }
         }
         System.out.println("music = "+ music);
---
+++ src/main/java/User.java
@@ -0,0 +1,80 @@
+import java.util.ArrayList;
+
+public class User {
+    private long chatId;
+    private String instrument;
+    private String genre;
+    private String pattern;
+    private int tempo;
+    private ArrayList<String> chords;
+    private boolean replaceChords;
+
+    public User(long chatId){
+        this.chatId=chatId;
+        instrument="DEFAULT";
+        genre="DEFAULT";
+        chords=new ArrayList<String>();
+        tempo=-1;
+        replaceChords=false;
+    }
+
+    public boolean isReplaceChords() {
+        return replaceChords;
+    }
+
+    public void setReplaceChords(boolean replaceChords) {
+        this.replaceChords = replaceChords;
+    }
+
+    public void addChord(String s){
+        chords.add(s);
+    }
+
+    public long getChatId() {
+        return chatId;
+    }
+
+    public void setChatId(long chatId) {
+        this.chatId = chatId;
+    }
+
+    public String getInstrument() {
+        return instrument;
+    }
+
+    public void setInstrument(String instrument) {
+        this.instrument = instrument;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getPattern() {
+        return pattern;
+    }
+
+    public void setPattern(String pattern) {
+        this.pattern = pattern;
+    }
+
+    public int getTempo() {
+        return tempo;
+    }
+
+    public void setTempo(int tempo) {
+        this.tempo = tempo;
+    }
+
+    public ArrayList<String> getChords() {
+        return chords;
+    }
+
+    public void setChords(ArrayList<String> chords) {
+        this.chords = chords;
+    }
+}
