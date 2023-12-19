aa5d7544c015272ef9637ae891ca954809d6dbef
khelix-san <82376512+khelix-san@users.noreply.github.com>
2023-12-19 17:43:20 +0100

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,4 @@
+In odrder for the scritp to work you need to install timidity and ffmpeg 
+To run the script, the .mid file must be in the same folder of the script and pass it's name (no extension) as input parameter
+example:
+Python3 converter.py test
---
+++ converter.py
@@ -0,0 +1,14 @@
+import os
+import sys
+
+# Accessing command-line arguments
+arguments = sys.argv
+
+fileName = arguments[1]
+
+try:
+    os.system("timidity "+fileName+".mid -Ow -o "+fileName+".wav")
+    os.system("ffmpeg -i "+fileName+".wav -y -vn -ar 44100 -ac 2 -ab 192k "+fileName+".mp3")
+    os.system("rm "+fileName+".mid;rm "+fileName+".wav")
+except Exception as e:
+    print("An error occurred:",e)
